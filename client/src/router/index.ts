3687194c8c7a4bd4e84872f31e112f95602b5f72
alessandro miele <alessandromiele94@hotmail.it>
2025-08-07 17:00:47 +0200

    feat: ts ignore on page import

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
+// @ts-ignore
 import MasterPage from '../pages/Master.vue';
+// @ts-ignore
 import PlayerPage from '../pages/Player.vue';
 
 const routes = [
